104abb89b5bb0130f073e4aca342b83cbea0d618
MikeLuan <luanxp@163.com>
2020-08-14 21:54:26 +0800

    修改了路径名

---
+++ src/main/java/com/luanxp/SpringIntegrationMqtt.java
@@ -0,0 +1,18 @@
+package com.luanxp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class SpringIntegrationMqtt
+{
+    public static void main( String[] args )
+    {
+
+        SpringApplication.run(SpringIntegrationMqtt.class,args);
+    }
+}
